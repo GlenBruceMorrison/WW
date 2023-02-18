0843e498349ae089baf946c7c56cc6e19ccc7010
glen.morrison <45969197+GlenBruceMorrison@users.noreply.github.com>
2023-02-18 14:01:56 +0000

    d

--- script.js
+++ script.js
@@ -9,7 +9,20 @@ function createScrollLink(linkName, targetName) {
 
 const sections = [
     "home",
-    "rsvp",
-    "location",
     "hotels"
 ]
+
+const activeSection = () => {
+    return document.querySelector('.active-section').id;
+}
+
+const setActiveSection = (x) => {
+    for (let element of sections) {
+      document.getElementById(element).style.display = 'none';
+      document.getElementById(`nav-${element}`).classList.remove('selected');
+    }
+
+    document.getElementById(sections[x]).style.display = 'block'
+    document.getElementById(`nav-${sections[x]}`).classList.add('selected');
+}
+
